dc4171667014e0cb38fa6170fa7436196dd15292
angmieee <37954765+angmieee@users.noreply.github.com>
2023-08-16 13:18:20 +0800

    Update vite.config.js
    自定义主题

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue2';
 import { resolve } from 'path';
 
+const themePath = path.resolve("src/styles/custom_theme.less")
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
@@ -15,6 +17,19 @@ export default defineConfig({
         },
     },
     resolve: {
-        alias: { find: /^~@vant/, replacement: resolve('node_modules/@vant') },
+        // 解决 vant 使用导入less样式使用 vite 不兼容的 "~@" 前缀的问题
+        { find: /^~/, replacement: "" },
     },
+    css: {
+        preprocessorOptions: {
+            less: {
+                modifyVars: {
+                    // 直接覆盖变量
+                    // "text-color": "#111",
+                    // 或者可以通过 less 文件覆盖（文件路径为绝对路径）
+                    hack: `true; @import "${themePath}";`
+                }
+            }
+        }
+    }
 });
